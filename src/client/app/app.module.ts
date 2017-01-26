8e8cb60c6f11adc1e938d932350113dc6889ab3d
jinhyungkim <haneu89@naver.com>
2017-01-26 10:39:54 +0900

    copy

---
+++ src/client/app/app.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { SharedModule } from '../shared/shared.module';
+
+import { AppRoutingModule } from './app.routing';
+import { AppComponent, XLargeDirective } from './app.component';
+
+
+@NgModule({
+    declarations: [ AppComponent, XLargeDirective ],
+    imports: [
+        SharedModule,
+        AppRoutingModule
+    ]
+})
+export class AppModule {
+}
+
+export { AppComponent } from './app.component';
